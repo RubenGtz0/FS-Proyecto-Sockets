80a63b256cf52ff2419255875a8bdf26df651014
RubenGtz0 <gutierrezruben.000@gmail.com>
2024-05-09 11:19:15 -0600

    Manejador Mensajes

---
+++ public/main.js
@@ -0,0 +1,39 @@
+/**
+ * Creamos la variable que permitira al frontend conectarse a nuestro backend
+ */
+var socket= io.connect('http://localhost:3002',{'forceNew':true});
+/*Esto manda al servidor el mensaje de connect y aparece console.log*/
+
+/**
+ * El cliente manejara datos mediante nesajes, estos se llamaran eventos y se mostraran
+ * por consola en el navegador
+ */
+socket.on('messages', function(data){
+    console.log(data);
+    render(data);
+});
+
+/* Creamos un template para que nos imprima el contenido*/
+
+function render (data){
+    //Aqui se inicia el manejo de string que viene en EM6 se usan estas comillas ´´
+    // Las variables se colocan con el signo de $ y entre {}
+    var html = `<div>
+    <strong> ${data.autor}</strong>:
+    <em>${data.texto}</em>
+    </div>`;
+
+    document.getElementById('messages').innerHTML=html;
+}
+//Despues renderizamos los datos actualizados en la consola
+
+//Cada vez que alguien precione el boton enviar en el formulario 
+//El cliente emite un nuevo mensaje y manda el payload
+function addMessage(e){
+    var payload = {
+        autor: document.getElementById(username).value,
+        texto: document.getElementById(texto).value
+    };
+    socket.emit('new-message', payload);
+    return false;
+}
---
+++ server/main.js
@@ -0,0 +1,33 @@
+var express = require ('express');
+var app= express();
+
+/* como trabajamos con socket, es recomendable usar el modulo HTTP para pasarle la app a express
+y manejar bien http */
+var server=require('http').Server(app);
+/**Aqui estara toda la funcionalidad de los sockets
+ * se requiere la libreria socket.io
+ * se pasa la variable Server que tiene la app express y http
+ * 
+*/
+var io= require('socket.io')(server);
+app.use(express.static ('public'));
+app.get('/', function (req, res){
+    res.status(200).send("Hola Mundo");
+});
+/**De esta forma activamos socket para que este escuchando mandamos un mensaje
+ * de control por consola para saber que pasa y tenemos que hacer que el mensaje 
+ * venga del navegador web mediante html y JS
+ */
+io.on('connection', function(socket){
+    console.log("Alguien se ha conectado con socket")
+    /**Aqui controlamos los eventod del cliente mediante sockets */
+    socket.emit('messages', {
+        id:1,
+        texto: "Un escalon arriba",
+        autor: "RG"
+    });
+});
+server.listen(3002, function(){
+    console.log("El servidor esta corriendo en http://localhost:3002");
+//
+});
